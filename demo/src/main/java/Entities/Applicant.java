5d50786afbd5efc1c8cc5fbc65496368c0787797
wejdan al-subhi <alwejdan97@gmail.com>
2026-06-15 09:44:45 +0400

    adding required feilds

--- demo/src/main/java/Entities/Applicant.java
+++ demo/src/main/java/Entities/Applicant.java
@@ -1,4 +1,19 @@
 package Entities;
+import Entities.Person;
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 
 public class Applicant extends Person{
+    private Integer id;
+    private String passportNumber;
+    private String nationality;
+    private Boolean criminalRecord;
+
 }
--- demo/src/main/java/Entities/Person.java
+++ demo/src/main/java/Entities/Person.java
@@ -1,6 +1,8 @@
 package Entities;
 
+import jakarta.persistence.MappedSuperclass;
 
+@MappedSuperclass
 public class Person {
     private String firstName;
     private String lastName;
