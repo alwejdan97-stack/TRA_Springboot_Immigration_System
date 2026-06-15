741f8bbe9adc1cfbf0484e2bfa1cdebf224fd455
wejdan al-subhi <alwejdan97@gmail.com>
2026-06-15 09:36:27 +0400

    adding constructors

--- demo/src/main/java/Entities/Person.java
+++ demo/src/main/java/Entities/Person.java
@@ -8,5 +8,11 @@ public class Person {
     String phoneNumber;
     String email;
 
-
+    public Person(String firstName, String lastName, String gender, String phoneNumber, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.gender = gender;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+    }
 }
