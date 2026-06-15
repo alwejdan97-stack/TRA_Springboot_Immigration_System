29edff14ee62bae787731af43f5036a4af7d1f9c
wejdan al-subhi <alwejdan97@gmail.com>
2026-06-15 10:19:51 +0400

    creat VisaApplications class and adding required feilds

--- demo/src/main/java/Entities/VisaApplications.java
+++ demo/src/main/java/Entities/VisaApplications.java
@@ -1,4 +1,17 @@
 package Entities;
 
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class VisaApplications {
+    private long id;
+    private String visaType;
+    private String status;
+    private String officerNotes;
 }
