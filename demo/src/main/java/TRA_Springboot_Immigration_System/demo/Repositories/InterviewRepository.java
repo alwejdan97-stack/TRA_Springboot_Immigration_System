1d312ae6ca95185f27e8bb99ccee70df4e95e056
wejdan al-subhi <alwejdan97@gmail.com>
2026-06-15 16:28:28 +0400

    add methods in InterviewService class

--- demo/src/main/java/TRA_Springboot_Immigration_System/demo/Repositories/InterviewRepository.java
+++ demo/src/main/java/TRA_Springboot_Immigration_System/demo/Repositories/InterviewRepository.java
@@ -2,11 +2,14 @@ package TRA_Springboot_Immigration_System.demo.Repositories;
 
 import TRA_Springboot_Immigration_System.demo.Entities.Interview;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface InterviewRepository extends JpaRepository<Interview,Long> {
-    List<Interview> findByOfficerIdAndInterviewDate(Long officerId, String date);
+    @Query("SELECT I FROM Interview I WHERE I.officer.id=:officer_id AND I.interviewDate=:interviewDate")
+    List<Interview> findByOfficerIdAndInterviewDate(@Param("officer_id") Long officerId, @Param("interviewDate") String date);
 }
--- demo/src/main/java/TRA_Springboot_Immigration_System/demo/Services/InterviewService.java
+++ demo/src/main/java/TRA_Springboot_Immigration_System/demo/Services/InterviewService.java
@@ -1,8 +1,58 @@
 package TRA_Springboot_Immigration_System.demo.Services;
 
 
+import TRA_Springboot_Immigration_System.demo.Entities.Applicant;
+import TRA_Springboot_Immigration_System.demo.Entities.ImmigrationOfficer;
+import TRA_Springboot_Immigration_System.demo.Entities.Interview;
+import TRA_Springboot_Immigration_System.demo.Repositories.ApplicantRepository;
+import TRA_Springboot_Immigration_System.demo.Repositories.InterviewRepository;
+import TRA_Springboot_Immigration_System.demo.Repositories.OfficerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class InterviewService {
+    InterviewRepository interviewRepository;
+    OfficerRepository officerRepository;
+    ApplicantRepository applicantRepository;
+
+    @Autowired
+    public InterviewService(InterviewRepository interviewRepository, OfficerRepository officerRepository,ApplicantRepository applicantRepository) {
+        this.interviewRepository = interviewRepository;
+        this.officerRepository = officerRepository;
+        this.applicantRepository=applicantRepository;
+    }
+
+    public Interview scheduleInterview(Long applicantId, Long officerId, String date){
+        Applicant applicant=applicantRepository.findById(applicantId).get();
+        ImmigrationOfficer officer=officerRepository.findById(officerId).get();
+        if(!officerRepository.existsById(officerId)){
+            throw new RuntimeException("Officer With ID "+officerId+" NOT Found...");
+        }
+        if(!applicantRepository.existsById(applicantId)){
+            throw new RuntimeException("Applicant With ID "+applicantId+" NOT Found...");
+        }
+        List<Interview> interviewList=interviewRepository.findByOfficerIdAndInterviewDate(officerId,date);
+        if(interviewList.isEmpty()){
+            Interview interview=new Interview();
+            interview.setApplicant(applicant);
+            interview.setOfficer(officer);
+            interview.setInterviewDate(date);
+            interview.setStatus("SCHEDULE");
+            return interviewRepository.save(interview);
+        }else{
+            throw new RuntimeException("Officer is double-booked!");
+        }
+    }
+
+    public Interview completeInterview(Long interviewId){
+        Interview interview=interviewRepository.findById(interviewId).get();
+        if(interviewRepository.existsById(interviewId)){
+            interview.setStatus("COMPLETE");
+            return interviewRepository.save(interview);
+        }
+        throw new RuntimeException("Interview With ID "+interviewId+" NOT Found...");
+    }
 }
