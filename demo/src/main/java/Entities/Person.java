aa629e5158abe1d689d84171314249023ab0222a
wejdan al-subhi <alwejdan97@gmail.com>
2026-06-15 09:36:56 +0400

    adding setter and getter methods

--- demo/src/main/java/Entities/Person.java
+++ demo/src/main/java/Entities/Person.java
@@ -15,4 +15,44 @@ public class Person {
         this.phoneNumber = phoneNumber;
         this.email = email;
     }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
