e38b2e2974f48f83e93ae436f8e27f06d2f7e829
wejdan al-subhi <alwejdan97@gmail.com>
2026-06-15 09:31:34 +0400

    adding required dependencies

---
+++ demo/src/main/java/TRA_Springboot_Immigration_System/demo/DemoApplication.java
@@ -0,0 +1,13 @@
+package TRA_Springboot_Immigration_System.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
---
+++ demo/src/test/java/TRA_Springboot_Immigration_System/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package TRA_Springboot_Immigration_System.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
