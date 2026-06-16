302c509bc031d5ae0e51634261feeff2260cf1e2
wejdan al-subhi <alwejdan97@gmail.com>
2026-06-16 08:28:24 +0400

    add constructors

--- demo/src/main/java/TRA_Springboot_Immigration_System/demo/Exceptions/ErrorResponse.java
+++ demo/src/main/java/TRA_Springboot_Immigration_System/demo/Exceptions/ErrorResponse.java
@@ -11,4 +11,13 @@ public class ErrorResponse {
     private String error;
     private String message;
     private String path;
+
+    public ErrorResponse(Date timestamp, int statusCode, HttpStatus status, String error, String message, String path) {
+        this.timestamp = timestamp;
+        this.statusCode = statusCode;
+        this.status = status;
+        this.error = error;
+        this.message = message;
+        this.path = path;
+    }
 }
